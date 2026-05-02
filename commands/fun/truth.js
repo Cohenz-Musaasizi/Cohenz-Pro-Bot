5322ae9286352ea0f0a6e41ff269d454e16d1111
Cohenz-Musaasizi <imacrecordz@gmail.com>
2026-05-02 16:18:28 +0300

    Update truth.js

--- commands/fun/truth.js
+++ commands/fun/truth.js
@@ -9,21 +9,22 @@ module.exports = {
     name: 'truth',
     aliases: [],
     category: 'fun',
-    desc: 'Get a random truth question',
-    usage: 'truth',
-    execute: async (sock, msg, args, extra) => {
+    description: 'Get a random truth question',
+    usage: '.truth',
+
+    async execute(sock, msg, args, context) {
+      const { reply } = context;
       try {
         const question = await truth();
         
         // Translate to English
         const res = await translate(question, { to: 'en' });
         
-        await extra.reply(res.text);
+        await reply(res.text);
         
       } catch (error) {
         console.error('Truth Error:', error);
-        await extra.reply(`❌ Error: ${error.message}`);
+        reply(`❌ Error: ${error.message}`);
       }
     }
-  };
-  
+};
