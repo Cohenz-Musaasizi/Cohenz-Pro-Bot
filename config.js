b18405ea45e30e1b12d804e4cece90d57fa1ea35
Cohenz-Musaasizi <imacrecordz@gmail.com>
2026-05-23 03:11:31 +0300

    Update config.js

--- config.js
+++ config.js
@@ -56,7 +56,7 @@ module.exports = {
     apiKeys: {
       openai: '',
       deepai: '',
-      gemini: 'AIzaSyC6q9C5I2cEaXBpIh8A7BQWk8OiY_ToHlA',   // <-- your Gemini key
+      gemini: process.env.GEMINI_API_KEY || '',   // <-- your Gemini key
       remove_bg: ''
     },
 
